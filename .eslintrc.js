98cbeff2b5459fdda7c46509c7f3a2846c46dd3b
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2022-06-12 18:53:39 +1000

    Initial commit with project setup; includes some dependencies.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off", // handled by TSC
+  },
+  ignorePatterns: [".eslintrc.js"],
+};
