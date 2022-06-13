8ce8dd285d925310e810e2b422a30d8ae5649ee7
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2022-06-13 17:18:40 +1000

    notification-stream.spec.ts: refactor for clarity, reusable composition

--- test/notification-stream.spec.ts
+++ test/notification-stream.spec.ts
@@ -9,52 +9,61 @@ class DisposableClient extends Client implements Disposable {
 }
 
 describe(NotificationStream.name, () => {
-  it(`streams values as an async iterable`, async () => {
-    const clientFactory = async () => {
-      const client = new DisposableClient({
-        connectionString:
-          "postgresql://postgres:password@127.0.0.1:5432/postgres",
+  const channel = "test_channel";
+  const testPayload = "Test Payload";
+
+  async function clientFactory() {
+    const client = new DisposableClient({
+      connectionString:
+        "postgresql://postgres:password@127.0.0.1:5432/postgres",
+    });
+    await client.connect();
+    return client;
+  }
+
+  async function sendNotification() {
+    await using(await clientFactory(), async (client) => {
+      await client.query(`NOTIFY "${channel}", '${testPayload}';`);
+    });
+  }
+
+  describe(`async iterable API`, () => {
+    async function iterateNotifications(
+      notificationStream: NotificationStream
+    ) {
+      const notifications: string[] = [];
+      for await (const notification of notificationStream) {
+        notifications.push(notification);
+      }
+      return notifications;
+    }
+
+    it(`streams values as an async iterable`, async () => {
+      await using(await clientFactory(), async (notificationClient) => {
+        const notificationStream = new NotificationStream(
+          notificationClient,
+          channel
+        );
+
+        // Start listening for messages
+        const iterablePromise = iterateNotifications(notificationStream).catch(
+          (err) => {
+            expect(err).toBeUndefined();
+          }
+        );
+
+        // Send a message
+        await sendNotification();
+
+        // Stop the notification stream
+        notificationStream.destroy();
+
+        // Wait for the iterable to end
+        const notifications = await iterablePromise;
+        await notificationClient.end();
+
+        expect(notifications).toEqual([testPayload]);
       });
-
-      await client.connect();
-
-      return client;
-    };
-
-    await using(await clientFactory(), async (notificationClient) => {
-      const channel = "test_channel";
-
-      const notificationStream = new NotificationStream(
-        notificationClient,
-        channel
-      );
-
-      // Start listening for messages
-      const iterablePromise = (async () => {
-        const notifications: string[] = [];
-        for await (const notification of notificationStream) {
-          notifications.push(notification);
-        }
-        return notifications;
-      })().catch((err) => {
-        expect(err).toBeUndefined();
-      });
-
-      // Send a message
-      const testPayload = "Test Payload";
-
-      await using(await clientFactory(), async (client) => {
-        await client.query(`NOTIFY "${channel}", '${testPayload}';`);
-      });
-
-      // Stop the notification stream
-      notificationStream.destroy();
-
-      // Wait for the iterable to end
-      const notifications = await iterablePromise;
-      await notificationClient.end();
-
-      expect(notifications).toEqual([testPayload]);
     });
   });
 });
