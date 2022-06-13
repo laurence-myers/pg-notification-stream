d445b0112df35b18deacec0592f943c24811be95
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2022-06-13 18:07:32 +1000

    notification-stream.spec.ts: add test for connection termination.

--- test/notification-stream.spec.ts
+++ test/notification-stream.spec.ts
@@ -3,8 +3,12 @@ import { NotificationStream } from "../src/notification-stream";
 import { Disposable, using } from "./using";
 
 class DisposableClient extends Client implements Disposable {
-  public dispose(): Promise<void> {
-    return this.end();
+  public async dispose(): Promise<void> {
+    try {
+      await this.end();
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
@@ -12,8 +16,9 @@ describe(NotificationStream.name, () => {
   const channel = "test_channel";
   const testPayload = "Test Payload";
 
-  async function clientFactory() {
+  async function clientFactory(applicationName: string) {
     const client = new DisposableClient({
+      application_name: applicationName,
       connectionString:
         "postgresql://postgres:password@127.0.0.1:5432/postgres",
     });
@@ -22,8 +27,19 @@ describe(NotificationStream.name, () => {
   }
 
   async function sendNotification() {
-    await using(await clientFactory(), async (client) => {
+    await using(await clientFactory("test_notifier"), async (client) => {
       await client.query(`NOTIFY "${channel}", '${testPayload}';`);
+    });
+  }
+
+  async function terminateConnection(applicationName = "test_listener") {
+    await using(await clientFactory("test_terminator"), async (client) => {
+      await client.query(
+        `SELECT pg_terminate_backend(pid)
+         FROM pg_stat_activity
+         WHERE application_name = $1`,
+        [applicationName]
+      );
     });
   }
 
@@ -39,31 +55,70 @@ describe(NotificationStream.name, () => {
     }
 
     it(`streams values as an async iterable`, async () => {
-      await using(await clientFactory(), async (notificationClient) => {
-        const notificationStream = new NotificationStream(
-          notificationClient,
-          channel
-        );
+      await using(
+        await clientFactory("test_listener"),
+        async (notificationClient) => {
+          const notificationStream = new NotificationStream(
+            notificationClient,
+            channel
+          );
 
-        // Start listening for messages
-        const iterablePromise = iterateNotifications(notificationStream).catch(
-          (err) => {
+          // Start listening for messages
+          const iterablePromise = iterateNotifications(
+            notificationStream
+          ).catch((err) => {
             expect(err).toBeUndefined();
-          }
-        );
+          });
 
-        // Send a message
-        await sendNotification();
+          // Send a message
+          await sendNotification();
 
-        // Stop the notification stream
-        notificationStream.destroy();
+          // Stop the notification stream
+          notificationStream.destroy();
 
-        // Wait for the iterable to end
-        const notifications = await iterablePromise;
-        await notificationClient.end();
+          // Wait for the iterable to end
+          const notifications = await iterablePromise;
+          await notificationClient.end();
 
-        expect(notifications).toEqual([testPayload]);
-      });
+          expect(notifications).toEqual([testPayload]);
+        }
+      );
+    });
+
+    it(`throws an error when the database connection unexpectedly disconnects`, async () => {
+      await using(
+        await clientFactory("test_listener"),
+        async (notificationClient) => {
+          const caughtClientErrors: unknown[] = [];
+          notificationClient.on("error", (err) => {
+            caughtClientErrors.push(err);
+          });
+
+          const notificationStream = new NotificationStream(
+            notificationClient,
+            channel
+          );
+
+          // Start listening for messages
+          let caughtIterableError: unknown;
+
+          const iterablePromise = iterateNotifications(
+            notificationStream
+          ).catch((err: unknown) => {
+            // Expecting a rejected promise doesn't work for some reason; the error is uncaught.
+            // Work around it by catching the error and checking it later.
+            caughtIterableError = err;
+          });
+
+          // Terminate the listener's DB connection
+          await terminateConnection();
+
+          // Wait for the iterable to end
+          await iterablePromise;
+          expect(caughtIterableError).toMatchSnapshot("iterable error");
+          expect(caughtClientErrors).toMatchSnapshot("client errors");
+        }
+      );
     });
   });
 });
